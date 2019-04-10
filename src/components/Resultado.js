d34a1db6383da4694e8415e4e11c652409b0b11e
armandoltx <armando@armandoltx.com>
2019-04-10 19:12:56 +1000

    Dandole estilo al Total

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class Resultado extends Component {
   render() {
+    const resultado = this.props.resultado;
+    const mensaje = (!resultado) ? 'Elije Marca, Ano y Seguro' : 'El total es: $';
+
+
     return (
-      <div>
-        {this.props.resultado}
+      <div className="gran-total">
+        {mensaje}
+        <TransitionGroup component='span' className="resultado">
+          <CSSTransition 
+            className="resultado"
+            key={resultado}
+            timeout={{enter: 500, exit: 500}}>
+            <span>{resultado}</span>
+          </CSSTransition>
+        </TransitionGroup>
       </div>
     );
   }
